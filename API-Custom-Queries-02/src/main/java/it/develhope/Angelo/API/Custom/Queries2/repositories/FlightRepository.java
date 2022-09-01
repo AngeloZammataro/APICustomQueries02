c0edbb326b6e59852b4b0ee5372979e30bef1f47
Angelo Zammataro <juba.kem@gmail.com>
2022-09-01 20:39:14 +0200

    Second Commit: API - Custom Query - 02

--- API-Custom-Queries-02/src/main/java/it/develhope/Angelo/API/Custom/Queries2/repositories/FlightRepository.java
+++ API-Custom-Queries-02/src/main/java/it/develhope/Angelo/API/Custom/Queries2/repositories/FlightRepository.java
@@ -10,11 +10,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Repository
 public interface FlightRepository extends JpaRepository<Flight,Long> {
 
 
