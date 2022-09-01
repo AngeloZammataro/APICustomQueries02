56517bf5c82c23cf1c78e6853ea249adcf6d2d33
Angelo Zammataro <juba.kem@gmail.com>
2022-09-01 21:07:22 +0200

    Third Commit: API - Custom Query - 02

--- API-Custom-Queries-02/src/main/java/it/develhope/Angelo/API/Custom/Queries2/repositories/FlightRepository.java
+++ API-Custom-Queries-02/src/main/java/it/develhope/Angelo/API/Custom/Queries2/repositories/FlightRepository.java
@@ -1,17 +1,12 @@
 package it.develhope.Angelo.API.Custom.Queries2.repositories;
 
-import it.develhope.Angelo.API.Custom.Queries2.controllers.FlightController;
 import it.develhope.Angelo.API.Custom.Queries2.entities.Flight;
 import it.develhope.Angelo.API.Custom.Queries2.entities.FlightStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
